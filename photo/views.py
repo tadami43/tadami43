db6af04adf1bd3a4854a0a8583954ccfdf16fa70
tadami <ami_tada@totec.co.jp>
2024-06-06 13:57:06 +0900

    44

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+# django.views.genericからTemplateViewをインポート
+from django.views.generic import TemplateView
 # Create your views here.
+
+class IndexView(TemplateView):
+    # トップページからのビュー
+    # index.htmlをレンダリングする
+    template_name = 'index.html'
