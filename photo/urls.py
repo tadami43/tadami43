02a90026773016a3edd830a20e0839794381d2c5
tadami <ami_tada@totec.co.jp>
2024-06-10 13:53:17 +0900

    147

--- photo/urls.py
+++ photo/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from . import views
 
-
+# URLパターンを逆引きできるように名前を付ける
 app_name = 'photo'
 
-
+# URLパターンを登録変数
 urlpatterns = [
+    # photoアプリへのアクセスは viewsモジュールのIndexViewを実行
     path('', views.IndexView.as_view(), name='index'),
 
+    # 写真投稿ページへのアクセスは viewsモジュールのCreatePhotoViewを実行
+    path('post/', views.CreatePhotoView.as_view(), name='post'),
+
+    #　投稿完了ページへのアクセスは viewsモジュールのPostSuccessViewを実行
+    path('post_done/', views.PostSuccessView.as_view(), name='post_done'),
 ]
--- photo/views.py
+++ photo/views.py
@@ -2,7 +2,7 @@ from django.forms import BaseModelForm
 from django.http import HttpResponse
 from django.shortcuts import render
 # django.views.genericからTemplateViewをインポート
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 # django.views.genericからCreateViewをインポート
 from django.views.generic import CreateView
 # django.urlsからreverse_lazyをインポート
@@ -13,10 +13,18 @@ from .forms import PhotoPostForm
 from django.utils.decorators import method_decorator
 # login_requiredをインポート
 from django.contrib.auth.decorators import login_required
+# modelsモジュールからモデルPhotoPostをインポート
+from .models import PhotoPost
 
-class IndexView(TemplateView):
+class IndexView(ListView):
+    '''トップページのビュー
+    '''
+    # index.htmlをレンダリングする
     template_name = 'index.html'
-
+    # モデルBlogPostのオブジェクトにorder_by()を適用して
+    # 投稿日時の降順で並べ替える
+    queryset = PhotoPost.objects.order_by('-posted_at')
+    
 
 # デコレーターにより、CreatePhotoViewへのアクセスはログインユーザーに限定される
 # ログイン状態でなければsettings.pyのLOGIN＿URLにリダイレクトされる
@@ -38,8 +46,10 @@ class CreatePhotoView(CreateView):
     template_name = "post_photo.html"
     # フォームデータ登録完了後のリダイレクト先
     success_url = reverse_lazy('photo:post_done')
+
     def form_valid(self, form):
         '''CreateViewクラスのform_valid()をオーバーライド
+        
         フォームのバリデーションを通過したときに呼ばれる
         フォームデータの登録をここで行う
         paramaters:
@@ -49,7 +59,7 @@ class CreatePhotoView(CreateView):
             HttpResponseRedirectオブジェクト:
                 スーパークラスのform_valid()に戻り値を返すことで、
                 success_urlで設定されているURLにリダイレクトされる
-                '''
+        '''
         # commit=FalseにしてPOSTされたデータを取得
         postdata = form.save(commit = False)
         # 投稿ユーザーのidを取得してモデルのuserフィールドに格納
@@ -58,3 +68,12 @@ class CreatePhotoView(CreateView):
         postdata.save()
         # 戻り値はスーパークラスのform_valid()の戻り値(HttpResponseRedirect)
         return super().form_valid(form)
+    
+class PostSuccessView(TemplateView):
+    '''投稿完了ページのビュー
+    
+    Attributes:
+        template_name:レンダリングするテンプレート
+    '''
+    # index.htmlをレンダリング
+    template_name = 'post_success.html'
